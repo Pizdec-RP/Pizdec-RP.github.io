2086af4482a7a0420baaf76b8e4e0e5caab84172
Pizdec-RP <qm0801u777@gmail.com>
2022-07-01 12:18:48 +0300

    хуй на

--- pixelplanet.fun-OverlayPicture/pixelPlanetOverlay-loader.user.js
+++ pixelplanet.fun-OverlayPicture/pixelPlanetOverlay-loader.user.js
@@ -1,12 +1,12 @@
 // ==UserScript==
 // @name         pixelplanet.fun picture overlay
-// @namespace    https://github.com/Woyken/pixelplanet.fun-OverlayPicture
+// @namespace    https://github.com/Pizdec-RP/pixelplanet.fun-OverlayPicture
 // @version      1.1.2
 // @description  Add your picture as overlay to pixelplanet.fun
-// @author       Woyken
+// @author       Pizdec-RP(spizdil code)
 // @include      https://pixelplanet.fun/*
 // @grant        none
-// @downloadURL  https://woyken.github.io/pixelplanet.fun-OverlayPicture/pixelPlanetOverlay-loader.user.js
+// @downloadURL  https://Pizdec-RP.github.io/pixelplanet.fun-OverlayPicture/pixelPlanetOverlay-loader.user.js
 // ==/UserScript==
 /**/
 
@@ -15,7 +15,7 @@
     if (window.location.host.startsWith("localhost"))
       e.src = new URL("src/userscript-loader-module/pixelPlanetOverlay.user.ts", window.location.href).href;
     else
-      e.src = "https://woyken.github.io/pixelplanet.fun-OverlayPicture/pixelPlanetOverlay.user.js";
+      e.src = "https://Pizdec-RP.github.io/pixelplanet.fun-OverlayPicture/pixelPlanetOverlay.user.js";
     e.type = "module";
     document.body.appendChild(e);
   }

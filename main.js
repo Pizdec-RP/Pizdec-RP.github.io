32983b7454c0217d316b602919afb36a2f94e625
Pizdec-RP <qm0801u777@gmail.com>
2022-10-27 20:11:17 +0300

    готово

--- main.js
+++ main.js
@@ -2,12 +2,24 @@ document.addEventListener('DOMContentLoaded',()=>{
     let world = document.querySelector(".main");
     let button = document.querySelector(".theme");
     let circle = document.querySelector(".circle");
-    let description = document.querySelector(".description");
-    let description2 = document.querySelector(".description2");
+    let description = document.querySelectorAll(".description");
+    
+    circle.classList.toggle("night");
+    world.classList.toggle("night");
+    description.forEach(element => {
+        element.classList.toggle("night");
+    });
+
     button.onclick = ()=>{
         circle.classList.toggle("night");
         world.classList.toggle("night");
-        description.classList.toggle("night");
-        description2.classList.toggle("night");
+        description.forEach(element => {
+            element.classList.toggle("night");
+        });
     }
 });
+
+function redirect() {
+    alert("Внимание!!!\nПереходя по ссылке вы подтверждаете что вы реально сексуальный мужик или просто крутая телка")
+    window.location.href = 'https:\/\/discord.gg/4ccHSE2CMq';
+}

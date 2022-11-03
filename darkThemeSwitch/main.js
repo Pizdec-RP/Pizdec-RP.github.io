a9afda6b818e7218befcd80470ae95de0351f768
Pizdec-RP <qm0801u777@gmail.com>
2022-11-03 14:32:37 +0300

    переключалка тем

--- darkThemeSwitch/main.js
+++ darkThemeSwitch/main.js
@@ -2,8 +2,31 @@ document.addEventListener('DOMContentLoaded',()=>{
     let world = document.querySelector(".world");
     let button = document.querySelector(".bckgcard");
     let circle = document.querySelector(".circle");
+    let box = document.querySelector(".box");
+    let tb = document.querySelector(".titlebox");
+    let title = document.querySelector(".title");
+    let text = document.querySelectorAll(".text");
+    let di = document.querySelector(".dopinfo");
+    let subtitle = document.querySelector(".subtitle");
     button.onclick = ()=>{
-        circle.classList.toggle("night");
-        world.classList.toggle("night");
+        n(circle);
+        n(world);
+        n(box);
+        n(tb);
+        n(title);
+        mn(text);
+        n(di);
+        n(subtitle);
     }
+
 });
+
+function n(classs) {
+    classs.classList.toggle("night");
+}
+
+function mn(classes) {
+    classes.forEach(element => {
+        element.classList.toggle("night");
+    });
+}

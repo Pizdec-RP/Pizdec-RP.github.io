92d24550b82dc61bd5f7c80a2b965aa7ddd0e0ac
Pizdec-RP <70069310+Pizdec-RP@users.noreply.github.com>
2022-11-10 10:11:25 +0300

    инв поменял

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
 
 function redirect() {
     alert("Внимание!!!\nПереходя по ссылке вы подтверждаете что вы реально сексуальный мужик или просто крутая телка")
-    window.location.href = 'https:\/\/discord.gg/4ccHSE2CMq';
+    window.location.href = 'https:\/\/discord.gg/EbDV2wrzv7';
 }
 
 function getssize() {

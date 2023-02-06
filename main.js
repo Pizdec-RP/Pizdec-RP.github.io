c4ebf9626ff642d54d95c16ff47112a793ffa88c
Pizdec-RP <qm0801u777@gmail.com>
2023-02-06 13:08:16 +0300

    pizdec

--- main.js
+++ main.js
@@ -103,9 +103,11 @@ var app = new Vue({
     }
 });
 
+alert("Внимание сервер только что ебнули модеры дса так что вот новый заходите https://discord.gg/U9KzkZduWs")
+
 function redirect() {
     alert("Внимание!!!\nПереходя по ссылке вы подтверждаете что вы реально сексуальный мужик или просто крутая телка")
-    window.location.href = 'https:\/\/discord.gg/EbDV2wrzv7';
+    window.location.href = 'https://discord.gg/U9KzkZduWs';
 }
 
 function getssize() {

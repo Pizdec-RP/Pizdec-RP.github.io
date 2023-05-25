e0a1beb9277d4e068fa2106e74934e02df653b94
Pizdec-RP <70069310+Pizdec-RP@users.noreply.github.com>
2023-05-25 11:14:00 +0300

    Update main.js

--- main.js
+++ main.js
@@ -139,7 +139,7 @@ var app = new Vue({
 
 function redirect() {
     alert("Внимание!!!\nПереходя по ссылке вы подтверждаете что вы реально сексуальный мужик или просто крутая телка")
-    window.location.href = 'https://discord.gg/h8M2JN5bjD';
+    window.location.href = 'https://discord.gg/KhQjvfRNVz';
 }
 
 function getssize() {
